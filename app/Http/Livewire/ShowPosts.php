6414bd737e82389489eab5a1ad252054fd42a280
unknown <wilberdaniel2@gmail.com>
2023-07-09 02:55:02 -0500

    se habilita la paginacion

--- app/Http/Livewire/ShowPosts.php
+++ app/Http/Livewire/ShowPosts.php
@@ -4,12 +4,46 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 use App\Models\Post;
+use Illuminate\Support\Facades\Storage;
+use Livewire\WithFileUploads;
+use Livewire\WithPagination;
+
 
 class ShowPosts extends Component
 {
-    public $search;
+
+    use WithFileUploads;
+    use WithPagination;
+
+
+    public $post, $image, $identificador;
+    public $search = '';
     public $sort = "id";
     public $direction = "desc";
+    public $cant = '10';
+
+    public $open_edit = false;
+
+    protected $queryString = [
+        'cant' =>['except' => '10'],
+        'sort'=>['except' =>'id'],
+        'direction'=>['except' =>'desc'],
+        'search'=>['except' =>'']
+    ];
+
+    public function mount(){
+        $this->identificador = rand();
+        $this->post = new Post();
+    }
+
+    public function updatingSearch(){
+        $this->resetPage();
+    }
+
+    protected $rules = [
+        'post.title' => 'required',
+        'post.content' => 'required'
+    ];
 
     protected $listeners = ['render'];
 
@@ -20,7 +54,7 @@ class ShowPosts extends Component
         $posts = Post::where('title', 'like', '%' . $this->search . '%')
                         ->orWhere('content', 'like', '%' . $this->search . '%')
                         ->orderBy($this->sort, $this->direction)
-                        ->get();
+                        ->paginate($this->cant);
 
         return view('livewire.show-posts', compact('posts'));
     }
@@ -39,5 +73,29 @@ class ShowPosts extends Component
             $this->direction = 'asc';
         }
         
+    }
+
+    public function edit(Post $post){
+        $this->post = $post;
+        $this->open_edit = true;
+    }
+
+    public function update(){
+
+        $this->validate();
+
+        if ($this->image) {
+            Storage::delete([$this->post->image]);
+
+            $this->post->image = $this->image->store('posts');
+        }
+
+        $this->post->save();
+
+        $this->reset(['open_edit', 'image']);
+
+        $this->identificador = rand();
+
+        $this->emit('alert', 'El post se ha actualizado satisfactoriamente');
     }
 }
--- resources/views/livewire/show-posts.blade.php
+++ resources/views/livewire/show-posts.blade.php
@@ -13,7 +13,20 @@
         <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
             <div class="px-6 py-4 flex items-center">
                 {{-- <input type="text" wire:model="search"> --}}
-                <x-jet-input class="flex-1 mr-4" placeholder="Escriba lo que quiera buscar" type="text" wire:model="search" />
+
+                <div class="flex items-center">
+                    <span>Mostrar</span>
+
+                    <select wire:model="cant" class="mx-2 form-control">
+                        <option value="10">10</option>
+                        <option value="25">25</option>
+                        <option value="50">50</option>
+                        <option value="100">100</option>
+                    </select>
+
+                    <span>Entradas</span>
+                </div>
+                <x-jet-input class="flex-1 mx-4" placeholder="Escriba lo que quiera buscar" type="text" wire:model="search" />
                 
                 @livewire('create-post')
             </div>
@@ -81,21 +94,24 @@
                     </tr>
                 </thead>
                 <tbody>
-                    @foreach ($posts as $post)
+                    @foreach ($posts as $item)
                         
                     <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
                         <th scope="row"
                             class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            {{$post->id}}
+                            {{$item->id}}
                         </th>
                         <td class="px-6 py-4">
-                            {{$post->title}}
+                            {{$item->title}}
                         </td>
                         <td class="px-6 py-4">
-                            {{$post->content}}
+                            {{$item->content}}
                         </td>
                         <td class="px-6 py-4">
-                            @livewire('edit-post', ['post' => $post], key($post->id))
+                            {{-- @livewire('edit-post', ['post' => $post], key($post->id)) --}}
+                            <a class="btn btn-green" wire:click="edit({{$item}})">
+                                <i class="fas fa-edit"></i>
+                            </a>
                         </td>
                     </tr>   
                     
@@ -111,6 +127,67 @@
                 
             @endif
 
+            @if ($posts->hasPages())
+
+            <div class="px-6 py-3">
+                {{$posts->links()}}
+            </div>
+                
+            @endif
+
+            
+
         </div>
     </div>
+
+
+    <x-jet-dialog-modal wire:model="open_edit">
+
+        <x-slot name="title">
+            Editar el post
+        </x-slot>
+
+        <x-slot name="content">
+
+            <div wire:loading wire:target="image" class="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
+                <span class="font-medium">Imagen cargando!</span> Espere hasta que la imagen se haya procesado.
+            </div>
+
+            @if ($image)
+                <img class="mb-4" src="{{$image->temporaryUrl()}}">
+
+            @else
+
+                <img src="{{Storage::url($post->image)}}" alt="">
+
+            @endif
+
+            <div class="mb-4">
+                <x-jet-label value="Titulo del post" />
+                <x-jet-input wire:model="post.title" type="text" class="w-full" />
+            </div>
+
+            <div>
+                <x-jet-label value="Contenido del post" />
+                <textarea wire:model="post.content" rows="6" class="form-control w-full"></textarea>
+            </div>
+
+            <div>
+                <input type="file" wire:model="image" id="{{$identificador}}">
+                <x-jet-input-error for="image" />
+            </div>
+
+        </x-slot>
+
+        <x-slot name="footer">
+            <x-jet-secondary-button wire:click="$set('open_edit', false)" class="mr-2">
+                Cancelar
+            </x-jet-secondary-button>
+
+            <x-jet-danger-button wire:click="update" wire:loading.attr="disabled" class="disabled:opacity-25">
+                Actualizar
+            </x-jet-danger-button>
+        </x-slot>
+
+    </x-jet-dialog-modal>
 </div>
